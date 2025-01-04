8fb0500dad6a81d27e55c4652424aff168c9aa3f
guzen <guzenko.dmitriy@gmail.com>
2025-01-04 15:02:43 +0200

    get

---
+++ src/main/java/com/guzenko/springcourse/dao/PersonDAO.java
@@ -0,0 +1,28 @@
+package com.guzenko.springcourse.dao;
+
+import com.guzenko.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private final List<Person> people;
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.get(id);
+    }
+}
